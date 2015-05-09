5f208eeb3060bb62d6b9e5d1cc532132629c2169
Daniel Mulholland <mulhollandd@jasmine>
2015-05-09 17:43:27 +1200

    Just keeping track of things
    Before partitioning for excel/terminal files

--- tp-setting-excel-tool.py
+++ tp-setting-excel-tool.py
@@ -22,11 +22,15 @@ Installation instructions (for Python *2.7.9*):
 
  - or if behind a proxy server: pip install --proxy="user:password@server:port" packagename
  - within Transpower: pip install --proxy="transpower\mulhollandd:password@tptpxy001.transpower.co.nz:8080" tablib    
-
+ 
+ C:\Python27\Scripts\pip install --proxy="transpower\mulhollandd:nottelling@tptpxy001.transpower.co.nz:8080" xlrd
+ 
 TODO: 
  - so many things
  - sorting options on display and dump output?    
  - sort out guessing of Transpower standard design version 
+ - sort out dumping all parameters
+ - sort out extraction of DNP data
 """
 
 __author__ = "Daniel Mulholland"
@@ -45,16 +49,55 @@ import sys
 import os
 import argparse
 import glob
-import re
+# should move to regex in Python 3 for compatibility
+import regex
 import tablib
-import openpyxl
 import xlrd
 import xlwt
+import openpyxl
 
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 OUTPUT_FILE_NAME = "output"
 OUTPUT_HEADERS = ['Filename','Setting Name','Val','Spreadsheet Reference']
 EXCEL_FILE_REGEX = '(xls|xlsx|xlsm)$'
+TXT_EXTENSION = 'TXT'
+SEL_EXPRESSION = r'[\w :+/\\()!,.\-_\\*]*'
+SEL_SETTING_EOL = r'\x1c\r\n'
+SEL_SETTING_NAME = r'[\w _]*'
+SEL_FID_EXPRESSION='^FID=([\w :+/\\()!,.\-_\\*]*)\r\n'
+
+SEL_EXPRESSION_RETURN = r'[\w :+/\\()!,.\-_\\*]'
+SEL_SETTING_NAME_RETURN = r'([A-Z0-9 _]{6})='
+SEL_FID_EXPRESSION='^FID=([\w :+/\\()!,.\-_\\*]*)\r\n'
+
+PARAMETER_SEPARATOR = ':'
+
+SEL_SEARCH_EXPR = {\
+    'G1': [['Group 1\r\nGroup Settings:', 'SELogic group 1\r\n'], \
+           ['$', '=>', 'Group [23456]\r\nGroup Settings:', 'SELogic group [23456]\r\n'] \
+          ], \
+    'G2': [['Group 2\r\nGroup Settings:', 'SELogic group 2\r\n'], \
+           ['$', '=>', 'Group [13456]\r\nGroup Settings:', 'SELogic group [13456]\r\n'] \
+          ], \
+    'G3': [['Group 3\r\nGroup Settings:', 'SELogic group 3\r\n'], \
+           ['$', '=>', 'Group [12456]\r\nGroup Settings:', 'SELogic group [12456]\r\n'] \
+          ], \
+    'G4': [['Group 4\r\nGroup Settings:', 'SELogic group 4\r\n'], \
+           ['$', '=>', 'Group [12356]\r\nGroup Settings:', 'SELogic group [12356]\r\n'] \
+          ],\
+    'G5': [['Group 5\r\nGroup Settings:', 'SELogic group 5\r\n'], \
+           ['$', '=>', 'Group [12346]\r\nGroup Settings:', 'SELogic group [12346]\r\n'] \
+          ], \
+    'G6': [['Group 6\r\nGroup Settings:', 'SELogic group 6\r\n'], \
+           ['$', '=>', 'Group [12345]\r\nGroup Settings:', 'SELogic group [12345]\r\n'] \
+          ], \
+    'P1': [['Port 1\r\n'], ['$', '=>', 'Port [2345F]\r\n']], \
+    'P2': [['Port 2\r\n'],['$', '=>', 'Port [2345F]\r\n']], \
+    'P3': [['Port 3\r\n'],['$', '=>', 'Port [2345F]\r\n']], \
+    'PF': [['Port F\r\n'],['$', '=>', 'Port [2345F]\r\n']], \
+    }
+
+OUTPUT_HEADERS = ['File','Group','Setting Name','Val']
 
 
 def main(arg=None):
@@ -72,12 +115,15 @@ def main(arg=None):
     parser.add_argument('path', metavar='PATH|FILE', nargs=1, 
                        help='Go recursively go through path PATH. Redundant if FILE'\
                        ' with extension .rdb is used. When recursively called, only'\
-                       ' searches for files with:' +  SR760_EXTENSION + '. Globbing is'\
+                       ' searches for files with:' +  TXT_EXTENSION + '. Globbing is'\
                        ' allowed with the * and ? characters.')
 
     parser.add_argument('-s', '--screen', action="store_true",
                        help='Show output to screen')
 
+    parser.add_argument('-a', '--all', action="store_true",
+                       help='Output all settings!')                       
+                       
     # Not implemented yet
     #parser.add_argument('-d', '--design', action="store_true",
     #                   help='Attempt to determine Transpower standard design version and' \
@@ -93,7 +139,9 @@ def main(arg=None):
                        ' '\
                        ' You can also get port settings using P:S'
                        ' Note: Applying a group for a non-grouped setting is unnecessary'\
-                       ' and will prevent you from receiving results.')
+                       ' and will prevent you from receiving results.'\
+                       ' Special parameters are the following self-explanatory items:'\
+                       ' FID, PARTNO, DEVID')
 
     parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
 
@@ -103,22 +151,12 @@ def main(arg=None):
         args = parser.parse_args(arg.split())
     
     # read in list of files
-    files_to_do = return_file_paths(args.path, SR760_EXTENSION)
+    files_to_do = return_file_paths(args.path, TXT_EXTENSION)
     
-    # sort out the reference data for addresses to parameter matching
-    lookup = {}
-    with open(INPUT_DATA_FILE, mode='r') as csvfile:
-        ref_d = csv.DictReader(csvfile)        
-        for row in ref_d:
-            key = row.pop('SR750/760 - V401') + ':' + row.pop('Info Name')
-            # we will assume no duplicates and ensure input data correct
-            #if key in result:
-            # implement your duplicate row handling here
-            #    pass
-            lookup[key] = row        
-    
+    # sort out the reference data for knowing where to search in the text string
+    lookup = SEL_SEARCH_EXPR
     if files_to_do != []:
-        process_760_files(files_to_do, args, lookup)
+        process_txt_files(files_to_do, args, lookup)
     else:
         print('Found nothing to do for path: ' + args.path[0])
         sys.exit()
@@ -158,7 +196,7 @@ def walkabout(p_or_f, file_extension):
                 return_files.append(os.path.join(root,name))
     return return_files
     
-def process_760_files(files_to_do, args, reference_data):
+def process_txt_files(files_to_do, args, reference_data):
     parameter_info = []
         
     for filename in files_to_do:      
@@ -191,47 +229,118 @@ def process_760_files(files_to_do, args, reference_data):
     if args.screen == True:
         display_info(parameter_info)
 
-def extract_parameters(filename, s_parameters, reference_data):
+def extract_parameters(filename, settings, reference_data):
     parameter_info=[]
-    
+
     # read data
     with open(filename,'r') as f:
         data = f.read()
 
-    # get input arguments in a sane format
-    s_parameters = re.findall(INPUT_SPLIT_EXPRESSION,' '.join(s_parameters))
+    """
+    How this regex works:
+     * (\r\n| |^)
+       - Looks for either a new line CR/LF  or a space or the start of the file.
+       - This is always true in process terminal views.
+     
+     * ([A-Z0-9 _]{6})
+       - SEL setting names are typically uppercase without spaces comprising 
+         characters A-Z 0-9 and sometimes with underscores (exception, DNP)
+     
+     * =
+       - Then followed by an equals character
+     
+     * (?>([\w :+/\\()!,.\-_\\*]+)
+       - There's quite a few options for what can be in a SEL expression
+       - This probably doesn't take them all into account add to suit 
+       - This is an atomic group expression which is a solution for making 
+         sure the delimiter doesn't get "eaten" because the delimiter is 
+         comprised of the same characters as the expression.
+         
+       - This is well described here: http://www.rexegg.com/regex-quantifiers.html
+     
+     * ([ ]{0}[A-Z0-9 _]{6}=|\r\n
+       - Then the delimiter comes. This is the next SEL setting name, if there
+         are multiple columns. Alternatively the delimiter is a newline CR/LF
+         combination.
+    """
 
-    # ^0x'17AA=([\w :+/\\()!,.\-_\\*]*)
-    # [SETPOINT GROUP 1](.|\n)*^0x17AA=([\w :+/\\()!,.\-_\\*]*)
-    result = None
-    for sp in s_parameters:
-        # there is only device information and setpoint data
-        grouping = sp.split(':')
-        val = re.search(r'^\[' \
-            + grouping[0].upper() \
-            + r'\]\r\n', data, flags=re.MULTILINE)
-        #print sp + " " + str(val.end())
-        if val is not None:  
-            if sp.split(':')[0] == 'Device Information':
-                result = re.search('^' + \
-                            reference_data[sp]['ADDRESS'] + \
-                             SR760_SETPOINT_EXPR_END, \
-                             data[val.end():], flags=re.MULTILINE).group(1)
+    """
+    TODO: This is how the --all or -a parameter should be implemented
+    results = regex.findall('(\r\n| |^)([A-Z0-9 _]{6})=(?>([\w :+/\\()!,.\-_\\*]+)([ ]{0}[A-Z0-9 _]{6}=|\r\n))', 
+        data, flags=regex.MULTILINE, overlapped=True)
+    """
+    
+    print settings
+    for parameter in settings:
+        # parameter is e.g. G1:50P1P
+        if parameter.find(PARAMETER_SEPARATOR) != -1:
+            grouper = parameter.split(PARAMETER_SEPARATOR)[0]
+            setting = parameter.split(PARAMETER_SEPARATOR)[1]
+            
+            print SEL_SEARCH_EXPR.keys()
+            if SEL_SEARCH_EXPR[grouper] == None:
+                print 'Searching the whole file without bounds'
+                pass
             else:
-                pattern = SR760_SETPOINT_EXPR_START + \
-                            reference_data[sp]['ADDRESS'] + \
-                             SR760_SETPOINT_EXPR_END
-                result = re.search(pattern, data[val.end():], 
-                    flags=re.MULTILINE).group(1)
-            #print [sp,result]
-        else:
-            print "Grouping term not found"
-        
-        if result <> None:
-            filename = os.path.basename(filename)
-            parameter_info.append([filename, sp, result])
+                start_regex = '('
+                for k in SEL_SEARCH_EXPR[grouper][0]:
+                    start_regex += k + '|'
+                start_regex += ')'
+                
+                end_regex = '('
+                for k in SEL_SEARCH_EXPR[grouper][1]:
+                    end_regex += k + '|'
+                end_regex += ')'
+                
+                print start_regex
+                print end_regex
+                
+                data2 = regex.find_all(start_regex+ ".*"+end_regex, data, flags = regex.MULTILINE)
+                print grouper
+                print setting
+                print data2
+            
+            
+            
+    sys.exit()
+                
+    # start_regex = 
+    # inner_regex = ljust(setting, 6, ' ')
+    # end_regex = 
+    
+    results = regex.findall('(\r\n| |^)([A-Z0-9 _]{6})=(?>([\w :+/\\()!,.\-_\\*]+)([ ]{0}[A-Z0-9 _]{6}=|\r\n))', 
+        data, flags=regex.MULTILINE, overlapped=True)
+    
+    #for m in matches:
+    #    print m[1].strip() + '=' + m[2].strip()
+    result = get_special_parameter('FID',data)
+    print result
+    result = get_special_parameter('PARTNO',data)
+    print result
+    result = get_special_parameter('DEVID',data)
+    print result
+    
+    if result <> None:
+        filename = os.path.basename(filename)
+        parameter_info.append([filename, m[1].strip(), m2.strip()])
             
     return parameter_info
+
+def get_special_parameter(name,data):
+    # Something like:
+    # name=FID for "FID=SEL-351S-6-R107-V0-Z003003-D20011129","0958"
+    # name=PARTNO for "PARTNO=0351S61H3351321","05AE"
+    # name=DEVID for "DEVID=TMU 2782","0402"
+    return regex.findall(r'^\"' + name + r'=([\w :+/\\()!,.\-_\\*\"]*\r\n)', 
+        data, flags=regex.MULTILINE, overlapped=True)
+
+def get_dnp(name, data):
+    # Not implemented yet
+    # Analogs  = 0 2 4 8 10 12 31 35 106 
+    # Binaries = 295 677 678 223 216 224 1020 1021 1022 296 527 571 567 595 735  \
+    #       734 233 242 740 251 179 180 181 360 361 362 863 364 865 866 867  \
+    #       767 766 765 679 680 681 864 
+    pass
 
 def display_info(parameter_info):
     lengths = []
@@ -250,11 +359,55 @@ def display_info(parameter_info):
         for index,element in enumerate(line):
             display_line += element.ljust(lengths[index]+2,' ')
         print display_line
-   
-if __name__ == '__main__':   
+
+def test_xls():
+
+    sys.stdout = open('log', 'w')
+    
+    my_file = return_file_paths(['in'], 'XLS')[0]
+    
+    workbook = xlrd.open_workbook(my_file)
+    print workbook.sheet_names()
+    # worksheet = workbook.sheet_by_name('Settings_Printout')
+    
+    worksheets = workbook.sheet_names()
+    for worksheet_name in worksheets:
+        worksheet = workbook.sheet_by_name(worksheet_name)
+        print worksheet.name
+    
+    workksheet = worksheets[0]
+    #worksheet = workbook.sheet_by_name('Group_1')
+    worksheet = workbook.sheet_by_name('Settings_Printout')
+    num_rows = worksheet.nrows - 1
+    curr_row = -1
+    while curr_row < num_rows:
+        curr_row += 1
+        row = worksheet.row(curr_row)
+        print row
+        
+    num_rows = worksheet.nrows - 1
+    num_cells = worksheet.ncols - 1
+    curr_row = -1
+    while curr_row < num_rows:
+        curr_row += 1
+        row = worksheet.row(curr_row)
+        print 'Row:', curr_row
+        curr_cell = -1
+        while curr_cell < num_cells:
+            curr_cell += 1
+            # Cell Types: 0=Empty, 1=Text, 2=Number, 3=Date, 4=Boolean, 5=Error, 6=Blank
+            cell_type = worksheet.cell_type(curr_row, curr_cell)
+            cell_value = worksheet.cell_value(curr_row, curr_cell)
+            print '	', cell_type, ':', cell_value
+        
+if __name__ == '__main__': 
     if len(sys.argv) == 1 :
-        main(r'-o xlsx in \
-        G1:TRIP G1:TR')
+        main(r'--all -o xlsx in G1:TR')
+        print 'hi'
+        
+        # test_xls()
+        
+        
     else:
         main()
     os.system("Pause")
